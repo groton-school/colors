8f2fca06e1cce4411c4b307f7990c5f6ab04861f
Seth Battis <sbattis@groton.org>
2025-09-16 08:24:28 -0400

    chore: update colorkit URL

--- src/Colors.ts
+++ src/Colors.ts
@@ -1,7 +1,7 @@
 export const no_color = 'rgb(128,128,128)'; // gray
 export const groton_red = '#a6093d';
 
-// https://colorkit.co/palette/ec111a-fb6330-ffd42f-29ad55-a1c3f2-2f2fa1-9353ed/
+// https://colorkit.co/palette/ec111a-fa8b2f-ffd42f-29ad55-a1c3f2-2f2fa1-9353ed/
 export const red = '#ec111a';
 export const orange = '#fa8b2f';
 export const yellow = '#ffd42f';
